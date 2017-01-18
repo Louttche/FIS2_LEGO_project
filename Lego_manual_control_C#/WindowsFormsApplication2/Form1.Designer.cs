c29ff0fe263c0157ed0cd928e37efa1a27c0fcc8
I369711 <d.karamanov@student.fontys.nl>
2017-01-18 15:01:48 +0100

    New Adding

---
+++ Lego_manual_control_C#/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.lb_Coloursensor = new System.Windows.Forms.Label();
+            this.lb_WallEsensor = new System.Windows.Forms.Label();
+            this.lb_status = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(51, 8);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(53, 31);
+            this.btnForward.TabIndex = 0;
+            this.btnForward.TabStop = false;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(22, 41);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(53, 31);
+            this.btnLeft.TabIndex = 1;
+            this.btnLeft.TabStop = false;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(81, 41);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(53, 31);
+            this.btnRight.TabIndex = 2;
+            this.btnRight.TabStop = false;
+            this.btnRight.Text = "Right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(51, 73);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(53, 31);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.TabStop = false;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Image = ((System.Drawing.Image)(resources.GetObject("btnUp.Image")));
+            this.btnUp.Location = new System.Drawing.Point(157, 34);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(39, 44);
+            this.btnUp.TabIndex = 4;
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Image = ((System.Drawing.Image)(resources.GetObject("btnDown.Image")));
+            this.btnDown.Location = new System.Drawing.Point(202, 34);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(39, 44);
+            this.btnDown.TabIndex = 5;
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // lb_Coloursensor
+            // 
+            this.lb_Coloursensor.AutoSize = true;
+            this.lb_Coloursensor.Location = new System.Drawing.Point(87, 22);
+            this.lb_Coloursensor.Name = "lb_Coloursensor";
+            this.lb_Coloursensor.Size = new System.Drawing.Size(0, 13);
+            this.lb_Coloursensor.TabIndex = 6;
+            // 
+            // lb_WallEsensor
+            // 
+            this.lb_WallEsensor.AutoSize = true;
+            this.lb_WallEsensor.Location = new System.Drawing.Point(74, 51);
+            this.lb_WallEsensor.Name = "lb_WallEsensor";
+            this.lb_WallEsensor.Size = new System.Drawing.Size(0, 13);
+            this.lb_WallEsensor.TabIndex = 7;
+            // 
+            // lb_status
+            // 
+            this.lb_status.AutoSize = true;
+            this.lb_status.Location = new System.Drawing.Point(69, 74);
+            this.lb_status.Name = "lb_status";
+            this.lb_status.Size = new System.Drawing.Size(0, 13);
+            this.lb_status.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.lb_Coloursensor);
+            this.groupBox1.Controls.Add(this.lb_status);
+            this.groupBox1.Controls.Add(this.lb_WallEsensor);
+            this.groupBox1.Location = new System.Drawing.Point(272, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(136, 96);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Colour Sensor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Status:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 26);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ultra-Sonic\r\nSensor:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Pick-Up Rotation";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 120);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnForward);
+            this.Name = "mainForm";
+            this.Text = "Robot Control";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Label lb_Coloursensor;
+        private System.Windows.Forms.Label lb_WallEsensor;
+        private System.Windows.Forms.Label lb_status;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lego_manual_control_C#/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+
+namespace WindowsFormsApplication2
+{
+    public partial class mainForm : Form
+    {
+        string idle = "IDLE";
+        string pickup = "Im PickingUP";
+        Brick _brick;
+        int _forward = 50;
+        int _backward = -50;
+        uint _time = 300;
+        //bool hello = false;
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void mainForm_Load(object sender, EventArgs e)
+        {
+            _brick = new Brick(new BluetoothCommunication("COM4"));
+            _brick.BrickChanged += _brick_BrickChanged;
+            await _brick.ConnectAsync();
+            await _brick.DirectCommand.PlayToneAsync(100, 1000, 300);
+            await _brick.DirectCommand.SetMotorPolarity(OutputPort.B | OutputPort.C, Polarity.Backward);
+            await _brick.DirectCommand.DrawTextAsync(Lego.Ev3.Core.Color.Background, 12, 32, "IM IDlE");
+            await _brick.DirectCommand.StopMotorAsync(OutputPort.All, false);
+            await _brick.DirectCommand.ClearChanges(InputPort.Three);
+            _brick.Ports[InputPort.Three].SetMode(TouchMode.Bumps);
+        }
+        
+        public void _brick_BrickChanged(object sender, BrickChangedEventArgs e)
+        {
+            lb_Coloursensor.Text = e.Ports[InputPort.One].SIValue.ToString();
+            lb_WallEsensor.Text = e.Ports[InputPort.Three].SIValue.ToString();
+            lb_status.Text = idle;
+        }
+
+        private async void btnForward_Click(object sender, EventArgs e)
+        {
+                await _brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.B | OutputPort.C, _forward, _time, false);
+                lb_status.Text = "on the RUN";
+        }
+
+        private async void btnBack_Click(object sender, EventArgs e)
+        {
+                await _brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.B | OutputPort.C, _backward, _time, false);
+                lb_status.Text = "on the RUN";
+        }
+
+        private async void btnLeft_Click(object sender, EventArgs e)
+        {
+            _brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, _backward, _time, false);
+            _brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.B, _forward, _time, false);
+             await _brick.BatchCommand.SendCommandAsync();
+            lb_status.Text = "on the RUN";
+        }
+
+        private async void btnRight_Click(object sender, EventArgs e)
+        {
+            _brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.B, _backward, _time, false);
+            _brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, _forward, _time, false);
+            await _brick.BatchCommand.SendCommandAsync();
+            lb_status.Text = "on the RUN";
+        }
+
+        private async void btnUp_Click(object sender, EventArgs e)
+        {
+                await _brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A, _forward, _time, false);
+                await _brick.DirectCommand.DrawTextAsync(Lego.Ev3.Core.Color.Background, 50, 50, "im in Pick UP MODE!!");
+                lb_status.Text = pickup;
+        }
+
+        private async void btnDown_Click(object sender, EventArgs e)
+        {
+                await _brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.A, _backward, _time, false);
+                await _brick.DirectCommand.DrawTextAsync(Lego.Ev3.Core.Color.Background, 50, 50, "I'm in Pick UP MODE!!");
+                lb_status.Text = pickup;
+        }
+    }
+}
